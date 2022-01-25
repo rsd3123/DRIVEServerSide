fb1b292f3bcd939a038e0088a68e5247c22e76a1
rsd3123 <57509739+rsd3123@users.noreply.github.com>
2022-01-25 15:53:59 -0500

    removed unused code

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
-/*
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1');
-});
-
-io.on('connection',(socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-});
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
-*/
 const static = require('node-static');
 const http = require('http');
 const file = new(static.Server)();
 const { Server } = require("socket.io");
+
 const app = http.createServer(function (req, res) {
   file.serve(req, res);
 }).listen(3000, ()=>{
@@ -36,17 +14,11 @@ const io = new Server(app);
 io.on('connection', (socket) => {
   var anotherSocketId = '';
   console.log('In socket: ',socket.id);
-  
-  /*
-  console.log("connected sockets-----:");
-  listSocketsProperty('data.name');
-  console.log("------------------");
-  */
- //console.log(socket.data.name);
 
   //When message is recieved by socket
   socket.on('message', (message) => {
     data2 = JSON.parse(message);
+
     switch (data2.type) {
       case 'login':
         //Automatically join empty room based off of socket.id
@@ -56,6 +28,7 @@ io.on('connection', (socket) => {
         console.log("Socket name: ", socket.name);
         console.log("New Socket ID: ", socket.id);
         break;
+
       //when somebody wants to call us
       case 'offer':
         console.log("Socket sending offer: ", socket.id, socket.name);
@@ -70,6 +43,7 @@ io.on('connection', (socket) => {
         anotherSocketId = '';
         console.log('Offer');
         break;
+
       case 'answer':
         //If answer = yes, join room of caller. If answer = no, dont join room, stay in current room.
         console.log("Socket sending answer: ", socket.id, socket.name);
@@ -79,6 +53,7 @@ io.on('connection', (socket) => {
         anotherSocketId = '';
         console.log('Answer');
         break;
+
       //when a remote peer sends an ice candidate to us
       case 'candidate':
         anotherSocketId = getSocketsProperty('name',data2.name);
@@ -88,12 +63,14 @@ io.on('connection', (socket) => {
         anotherSocketId = '';
         console.log('Candidate');
         break;
+
       case 'leave':
         //socket.to(anotherSocketId).emit("leave", socket.id, message);
         //Disconnect socket from current room
         console.log('Leave');
         socket.disconnect();
         break;
+
       default:
         break;
     }
@@ -105,6 +82,7 @@ io.on('connection', (socket) => {
     });
   });
 
+  /* Unused function 
   function listSocketsProperty(myProperty){
     let sck = io.sockets.sockets
     const mapIter = sck.entries()
@@ -115,6 +93,7 @@ io.on('connection', (socket) => {
       else break
     }
   }
+  */
 
   //Return socket id based off of a property
   function getSocketsProperty(property, target){
@@ -132,29 +111,4 @@ io.on('connection', (socket) => {
       else break
     }
   }
-
-  /*
-  socket.on('create or join', (room) => {
-    const numClients = io.sockets.clients(room).length;
-
-    console.log('Room ' + room + ' has ' + numClients + ' client(s)');
-    console.log('Request to create or join room ' + room);
-
-    if (numClients === 0){
-      socket.join(room);
-      socket.emit('created', room);
-    } else if (numClients === 1) {
-      io.sockets.in(room).emit('join', room);
-      socket.join(room);
-      socket.emit('joined', room);
-    } else { // max two clients
-      socket.emit('full', room);
-    }
-    socket.emit('emit(): client ' + socket.id +
-      ' joined room ' + room);
-    socket.broadcast.emit('broadcast(): client ' + socket.id +
-      ' joined room ' + room);
-
-  });
-  */
 });
